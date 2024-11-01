c3e66d455cb69fcda1d18172b776b8f865cf20d5
CodeX042 <codexteam042@gmail.com>
2024-11-01 16:39:23 +0100

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>METAMUSE</span>,
+  project: {
+    link: 'https://github.com/CodeX042',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/CodeX042',
+  footer: {
+    text: 'BY CodeX',
+  },
+}
+
+export default config
