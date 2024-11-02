2726961264179c6d279c1f336f76caa94597ea54
GOLDMAN <49483043+georgegoldman@users.noreply.github.com>
2024-11-02 13:56:34 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>METAMUSE</span>,
   project: {
-    link: 'https://github.com/CodeX042',
+    link: 'https://github.com/CodeX042/METAMUSE.git',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/CodeX042',
+  docsRepositoryBase: 'https://github.com/CodeX042/METAMUSE.git',
   footer: {
     text: 'BY CodeX',
   },
